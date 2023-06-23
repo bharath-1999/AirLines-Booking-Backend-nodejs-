0accc86aa9d9f53c0a7575cbc1df7df627d98d0c
Bharath Kumar <bharathkumar05599@gmail.com>
2023-06-23 12:39:10 +0530

    added filters to get all services

--- src/respositories/city-repository.js
+++ src/respositories/city-repository.js
@@ -1,5 +1,5 @@
 const { City } = require('../models/index')
-
+const { Op } = require('sequelize')
 class CityRepository {
     async createCity({ name }) {
         try{
@@ -44,8 +44,18 @@ class CityRepository {
             throw {error};
         }
     }
-    async getAllCities(cityId){
+    async getAllCities(filter){
         try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith] : filter.name 
+                        }
+                    }
+                })
+                return cities;
+            }
             const cities = await City.findAll()
             return(cities)
         } catch (error) {
